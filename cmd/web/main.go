c7300d4f0b11d1e61542b143112d518dbd9be18d
gAzoth <johnhgaffney@gmail.com>
2025-06-03 20:18:18 -0400

    template cache made

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/gAzoth/resumeSite/pkg/config"
 	"github.com/gAzoth/resumeSite/pkg/handlers"
+	"github.com/gAzoth/resumeSite/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main function
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
 
-	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	fmt.Printf("Staring application on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,15 +3,44 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/gAzoth/resumeSite/pkg/config"
+	"github.com/gAzoth/resumeSite/pkg/models"
 	"github.com/gAzoth/resumeSite/pkg/render"
 )
 
+// Repo is the repository used by the handlers
+var Repo *Repository
+
+// Repository if the new repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // Home is the handler for the home page
-func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the handler for the about page
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+
+	//perform logic
+	stringMap := map[string]string{}
+	stringMap["test"] = "Hello, again."
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
